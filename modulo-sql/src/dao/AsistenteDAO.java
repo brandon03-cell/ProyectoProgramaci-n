38e94559187564dccf08d28d16526f52aa2419a6
brandon03-cell <brandonchusgo2003@gmail.com>
2026-05-19 19:54:12 +0200

    starting new class bro

--- modulo-sql/src/dao/AsistenteDAO.java
+++ modulo-sql/src/dao/AsistenteDAO.java
@@ -1,4 +1,9 @@
 package dao;
 
 public class AsistenteDAO {
+    private String url = "jdbc:mysql://localhost:3308/proyectoProgramacion";
+    private String user = "root";
+    private String password = "Brandon_01052003";
+
+
 }
