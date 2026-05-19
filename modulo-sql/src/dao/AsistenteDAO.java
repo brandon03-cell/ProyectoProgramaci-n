40db4a80a8ed0e1f92350ed7befde16acb1ac68b
brandon03-cell <brandonchusgo2003@gmail.com>
2026-05-19 20:12:59 +0200

    Rafael fresno

--- modulo-sql/src/dao/AsistenteDAO.java
+++ modulo-sql/src/dao/AsistenteDAO.java
@@ -1,9 +1,27 @@
 package dao;
 
+import modelo.Asistente;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class AsistenteDAO {
     private String url = "jdbc:mysql://localhost:3308/proyectoProgramacion";
     private String user = "root";
     private String password = "Brandon_01052003";
 
-
+    public void insertarAsistente(Asistente a) {
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            String sql = "insert into asistentes (nombre, email, edad) values (?, ?, ?)";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, a.getNombre());
+            ps.setString(2, a.getEmail());
+            ps.setInt(3, a.getEdad());
+            ps.executeUpdate();
+        } catch (SQLException exception) {
+            System.out.println("Erro ao inserir asistente: " + exception.getMessage());
+        }
+    }
 }
