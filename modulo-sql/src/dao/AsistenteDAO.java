404eaffd4b85841bff4f4f30d852e7a39e6ee7fb
brandon03-cell <brandonchusgo2003@gmail.com>
2026-05-19 20:15:13 +0200

    xd

--- modulo-sql/src/dao/AsistenteDAO.java
+++ modulo-sql/src/dao/AsistenteDAO.java
@@ -24,4 +24,18 @@ public class AsistenteDAO {
             System.out.println("Erro ao inserir asistente: " + exception.getMessage());
         }
     }
+
+    public void actualizarAsistente(Asistente a) {
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            String sql = "update asistentes set nombre = ?, email = ?, edad = ? where id = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, a.getNombre());
+            ps.setString(2, a.getEmail());
+            ps.setInt(3, a.getEdad());
+            ps.setInt(4, a.getId());
+            ps.executeUpdate();
+        } catch (SQLException exception) {
+            System.out.println("Erro ao actualizar asistente: " + exception.getMessage());
+        }
+    }
 }
